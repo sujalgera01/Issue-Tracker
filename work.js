590ca9ac3d52daa14487d11c5f07d2418ee5bba6
sujalgera01 <sujalgera01@gmail.com>
2020-05-25 16:40:49 +0530

    initial commits

---
+++ work.js
@@ -0,0 +1,60 @@
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue);
+
+function saveIssue(e){
+	var issueDesc = document.getElementById('issueDescInput').value;
+	var issueSeverity = document.getElementById('issueSeverityInput').value;
+	var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+	var issueId = chance.guid();
+	var issueStatus = 'open';
+
+	var issue = {
+		id: issueId,
+		description: issueDesc,
+		severity: issueSeverity,
+		assignedTo: issueAssignedTo,
+		status: issueStatus
+	}
+
+	if(localStorage.getItem('issues') == null){
+		var issues =  [];
+		issues.push(issue);
+		localStorage.setItem('issues',JSON.stringify(issues));
+	}
+	else{
+		var issues = JSON.parse(localStorage.getItem('issues'));
+		issues.push(issue);
+		localStorage.setItem('issues',JSON.stringify(issues));
+	}
+
+	document.getElementById('issueInputForm').reset();
+
+	fetchIssues();
+
+	e.preventDefault();
+}	
+
+
+function fetchIssues(){
+	var issues = JSON.parse(localStorage.getItem('issues'));
+	var issuesListe = document.getElementById('issuesList');
+
+	issuesList.innerHTML = '';
+
+	for(var i=0; i< issues.length; i++){
+		var id = issues[i].id;
+		var desc = issues[i].description;
+		var severity = issues[i].severity;
+		var assignedTo = issues[i].assignedTo;
+		var status = issues[i].status;
+
+		issuesList.innerHTML += '<div class = "well">'+
+								'<h6>Issue Id: ' + id + '</h6>' +
+								'<p><span class= "label label-info">' + status + '</span></p>'+
+								'<h3>' + desc + '</h3>'+
+								'<p><span class= "glyphicon glyphicon-time"></span>' + severity + '</p>'+
+								'<p><span class= "glyphicon glyphicon-user"></span>' + assignedTo + '</p>'+
+								'<a onclick="setStatusClosed(\''+id+'\')" href="#" class="btn btn-warning">close</a>'+
+								'<a onclick="deleteIssue(\''+id+'\') href="#" class="btn btn-danger">Delete</a>'+
+								'</div>';
+	}
+}
